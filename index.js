12af883c4f2e48e64709704cc93fcc7bfd5c0bdc
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2023-07-31 01:04:54 +0530

    Add conditional logic ⚖

--- index.js
+++ index.js
@@ -7,12 +7,19 @@
  *  });
  *
  * @param {function} callback function(val, idx, obj)
+ * @param {boolean} concurrent
  */
 
 if (!Array.prototype.forEachAsync) {
-    Array.prototype.forEachAsync = async function (callback) {
-        await Promise.all(this.map((item, index) => {
-            return callback(item, index, this);
-        }));
+    Array.prototype.forEachAsync = async function (callback, concurrent = false) {
+        if (!!concurrent) {
+            await Promise.all(this.map((item, index) => {
+                return callback(item, index, this);
+            }));
+        } else {
+            for (let index = 0; index < this.length; index++) {
+                await callback(this[index], index, this);
+            }
+        }
     };
 }
